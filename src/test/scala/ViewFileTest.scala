fb84332b09b20f55b568d9f78620e6439c724cb5
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2019-05-11 22:03:19 -0700

    Tests for extensions with popup

---
+++ src/test/scala/ViewFileTest.scala
@@ -0,0 +1,89 @@
+import base.BrowserTest
+import org.openqa.selenium.Keys
+import utest._
+import scala.collection.JavaConverters._
+
+object ViewFileTest extends BrowserTest {
+  val tests = Tests {
+    "jump to a definition" - {
+      go("https://github.com/tanin47/test-java-repo/blob/d022270ccb8f346cb8ef2136c4212ff71802eff5/src/main/java/test_java_repo/Main.java")
+
+      val usage = "#LC150 .lilit-link"
+      waitUntil { usage.items.nonEmpty }
+
+      usage.hover()
+
+      waitUntil { usage.getAttribute("aria-describedby") != null }
+      s"#${usage.getAttribute("aria-describedby")}".getText ==> "Defined in this file on the line 153"
+
+      usage.click()
+
+      val definition = "#LC153 .lilit-link"
+      waitUntil { definition.items.nonEmpty }
+
+      webDriver.getCurrentUrl.contains("#L153") ==> true
+      "#LC150".getAttribute("class").contains("lilit-highlighted") ==> true
+      "#LC153".getAttribute("class").contains("lilit-highlighted") ==> true
+    }
+
+    "See all usages and jump to one" - {
+      go("https://github.com/tanin47/test-java-repo/blob/d022270ccb8f346cb8ef2136c4212ff71802eff5/src/main/java/test_java_repo/Main.java")
+
+      val definition = "#LC153 .lilit-link"
+      waitUntil { definition.items.nonEmpty }
+
+      definition.hover()
+
+      waitUntil { definition.getAttribute("aria-describedby") != null }
+      s"#${definition.getAttribute("aria-describedby")}".getText.trim ==>
+        """
+          |Found 1 occurrence in this file
+          |Found 1 occurrence in src/test/java/test_java_repo/MainTest.java
+        """.stripMargin.trim
+
+      definition.click()
+
+      waitUntil { webDriver.getCurrentUrl.startsWith("https://lilit.dev") }
+
+      ".content ul li a".items(1).click()
+
+      waitUntil { webDriver.getCurrentUrl.startsWith("https://github.com") }
+      waitUntil { ".lilit-link".nonEmpty }
+
+      webDriver.getCurrentUrl.contains("#L10") ==> true
+      "#LC10".getAttribute("class").contains("lilit-highlighted") ==> true
+    }
+
+    "Request to index a commit" - {
+      go("https://github.com/tanin47/test-java-repo/blob/b0572fe9bd5e7b4f33ddd1290dfb4f3a266ac3d7/src/main/java/test_java_repo/Main.java")
+
+      webDriver.switchTo().window(openNewTab())
+      go("chrome-extension://fahmaaghhglfmonjliepjlchgpgfmobi/popup.html?test")
+
+      waitUntil { "#requestPanel".getText.contains("The commit(s), b0572f, aren't indexed by Lilit.") }
+
+      "#requestButton".click()
+
+      waitUntil {
+        "#requestPanel".getText.contains(
+          """
+            |We've received your request. Indexing a commit can take up to 10 minutes. Please later reload the page to see if the commits have been supported.
+          """.stripMargin.trim
+        )
+      }
+
+      "#requestButton".isRendered() ==> false
+    }
+
+    "See an unsupported repo" - {
+      go("https://github.com/tanin47/unsupported-java-repo/blob/master/Test.java")
+
+      webDriver.switchTo().window(openNewTab())
+      go("chrome-extension://fahmaaghhglfmonjliepjlchgpgfmobi/popup.html?test")
+
+      waitUntil {
+        "#failureReasonText".getText.trim == "tanin47/unsupported-java-repo isn't supported. See what to do here."
+      }
+    }
+  }
+}
